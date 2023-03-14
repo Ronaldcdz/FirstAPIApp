e98b0bcbc2cf0180fa38413b625a3899e5363ef1
Ronald Cadiz <ronaldcdz26@gmail.com>
2023-03-13 23:41:07 -0400

    Structure changed

---
+++ Database/Common/AuditableBaseEntity.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Database.Common
+{
+    public class AuditableBaseEntity
+    {
+        public int Id;
+        public DateTime CreatedAt;
+        public DateTime IpdatedAt;
+
+
+
+    }
+}
---
+++ Database/Models/Book.cs
@@ -0,0 +1,22 @@
+﻿using Database.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Database.Models
+{
+    public class Book : AuditableBaseEntity
+    {
+        public string ImagePath;
+        public string Title;
+        public string Description;
+        public int Rating;
+        public double Price;
+        public int Pages;
+        public int CategoryId;
+        public int AuthorId;
+
+    }
+}
