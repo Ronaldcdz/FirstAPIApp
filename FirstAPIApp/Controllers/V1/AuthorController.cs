7b6fff612fd91bb2f8b70ccf6c092cfb7cb79f96
Ronald Cadiz <ronaldcdz26@gmail.com>
2023-03-16 18:11:50 -0400

    API Controllers Added

---
+++ FirstAPIApp/Controllers/V1/AuthorController.cs
@@ -0,0 +1,42 @@
+﻿using Database;
+using Database.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Reflection.Metadata.Ecma335;
+
+namespace FirstAPIApp.Controllers.V1
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthorController : ControllerBase
+    {
+        private readonly ApplicationDbContext _dbContext;
+
+        private static List<Author> authors = new List<Author>
+            {
+                new Author
+                {
+                    Id= 1,
+                    Name = "James Clear",
+                    BornAt = "26/02/2002"
+                }
+            };
+
+
+        public AuthorController(ApplicationDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        [HttpGet]
+        [Route("Get")]
+        public async Task<ActionResult<List<Author>>> Get()
+        {
+            
+
+            return StatusCode(StatusCodes.Status200OK, authors);
+            //return Ok(authors);
+        }
+    }
+}
---
+++ FirstAPIApp/Controllers/V1/BookController.cs
@@ -0,0 +1,21 @@
+﻿using Database;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FirstAPIApp.Controllers.V1
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BookController : ControllerBase
+    {
+        private readonly ApplicationDbContext _dbContext;
+
+        public BookController(ApplicationDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+
+
+    }
+}
